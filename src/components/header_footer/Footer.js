12f15b2fea59fab6983503534435bb2e36860694
Bruno Gualberto <67048428+Bruno-Gualberto@users.noreply.github.com>
2021-07-09 11:31:10 +0200

    add logos and footer

--- src/components/header_footer/Footer.js
+++ src/components/header_footer/Footer.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { CityLogo } from '../utils/tools';
 
 const Footer = () => {
   return (
-    <>
-      Footer
-    </>
+    <footer className="bck_blue">
+      <div className="footer_logo">
+        <CityLogo
+          link={true}
+          linkTo={'/'}
+          width="70px"
+          height="70px"
+        />
+      </div>
+      <div className="footer_discl">
+        Manchester City. All rights reserved
+      </div>
+    </footer>
   )
 }
 
